a795fee1c9e0a94888c3b59d4cdd9f81b71f3561
Netkiller <lpuentesrivas@gmail.com>
2024-11-24 15:42:17 -0500

    contact and home texts

--- home/models.py
+++ home/models.py
@@ -8,4 +8,7 @@ class Layout(models.Model):
     navbar_color = models.CharField(max_length=50)
     bg_color = models.CharField(max_length=50)
     footer_color = models.CharField(max_length=50)
+    home_html = models.TextField()
+    contact_html = models.TextField()
     in_use = models.BooleanField()
+    
