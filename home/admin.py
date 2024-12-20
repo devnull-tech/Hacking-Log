b731dc6d86b0413f27ee57334adc1e6b33804bae
devnull-tech <lpuentesr@gmail.com>
2024-12-20 00:12:16 -0500

    VisitorAdmin readonly fix

--- home/admin.py
+++ home/admin.py
@@ -7,6 +7,15 @@ class LayoutAdmin(admin.ModelAdmin):
 class VisitorAdmin(admin.ModelAdmin):
     list_display = ['ip_address', 'url', 'timestamp']
     readonly_fields = ['ip_address', 'url', 'timestamp']
+    
+    def has_add_permission(self, request):
+        return False
+    
+    def has_delete_permission(self, request, obj = ...):
+        return False
+    
+    def has_change_permission(self, request, obj = ...):
+        return False
 
 admin.site.register(Layout, LayoutAdmin)
 admin.site.register(Visitor, VisitorAdmin)
