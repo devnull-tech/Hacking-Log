7aa6428322008e5fca701d66ecd75d0fbc63354f
devnull-tech <lpuentesr@gmail.com>
2024-12-19 22:04:09 -0500

    layouts model changed

--- home/models.py
+++ home/models.py
@@ -4,11 +4,10 @@ class Layout(models.Model):
     layout_name = models.CharField(max_length=100)
     base64_banner = models.TextField()
     title = models.CharField(max_length=30)
-    text_color = models.CharField(max_length=50)
-    navbar_color = models.CharField(max_length=50)
-    bg_color = models.CharField(max_length=50)
-    footer_color = models.CharField(max_length=50)
     home_html = models.TextField()
     contact_html = models.TextField()
     in_use = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.layout_name
     
