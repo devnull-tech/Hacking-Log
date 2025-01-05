9d2a069a88143a62bcc79e9d863cca7f996205d4
devnull-tech <lpuentesr@gmail.com>
2025-01-05 13:43:44 +0100

    `Removed bot model, and form`

--- hacklog/models.py
+++ hacklog/models.py
@@ -8,39 +8,6 @@ class Tip(models.Model):
     def __str__(self):
         return self.title
 
-class Bot(models.Model):
-    provider = models.CharField(max_length=50)
-    model = models.CharField(max_length=100)
-    system_prompt = models.CharField(max_length=200)
-    temperature = models.FloatField()
-    base_url = models.CharField(max_length=50)
-    api_key = models.CharField(max_length=200)
-    input_max_tokens = models.IntegerField()
-    in_use = models.BooleanField(default=False)
-    
-    def send_prompt(self, prompt: str) -> dict: 
-        client = OpenAI(
-            api_key=self.api_key, 
-            base_url=self.base_url
-        )
-        completion = client.chat.completions.create(
-            messages = [
-                {"role": "system", "content": self.system_prompt},
-                {"role": "user", "content": prompt},
-            ], 
-            model = self.model,
-            temperature = self.temperature
-        )
-        return {
-            'text': completion.choices[0].message.content,
-            'prompt_tokens': completion.usage.prompt_tokens,
-            'response_tokens': completion.usage.completion_tokens,
-            'bot': self
-        }
-        
-    def __str__(self):
-        return self.provider
-
 class Term(models.Model):
     term = models.CharField(max_length=255)
     description = models.TextField()
--- hacklog/views.py
+++ hacklog/views.py
@@ -21,21 +21,10 @@ def index(request):
 def log(request, log_id):
     layout_data = Layout.objects.filter(in_use=True).first()
     log = get_object_or_404(models.Log, id=log_id, is_public=True)
-    if request.method == 'GET':
-        return render(request, 'hacklog/log.html', {
-            'layout_data': layout_data,
-            'log': log
-        })
-    if request.method == 'POST':
-        question = request.POST.get('question')
-        bot = get_list_or_404(models.Bot, in_use=True)[0]
-        answer = bot.send_prompt(question)
-        return render(request, 'hacklog/log.html', {
-            'layout_data': layout_data,
-            'log': log,
-            'question':question,
-            'answer': answer['text']
-        })
+    return render(request, 'hacklog/log.html', {
+        'layout_data': layout_data,
+        'log': log
+    })
 
 def glosary(request):
     layout_data = Layout.objects.filter(in_use=True).first()
